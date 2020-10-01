b1db04f11afcbf4bdfa306a41d0c69185e518b9a
Deep <66822101+deepshah18@users.noreply.github.com>
2020-09-30 23:36:21 -1200

    Add files via upload

---
+++ AdminLogin.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void login_btn_Click(object sender, EventArgs e)
+    {
+        if ((uid_txt.Text.Equals("Admin")) && (pwd_txt.Text.Equals("Admin")))
+        {
+            Response.Redirect("~/Admin/Home.aspx");
+        }
+        else if ((uid_txt.Text.Equals("admin")) && (pwd_txt.Text.Equals("admin")))
+        {
+            Response.Redirect("~/Admin/Home.aspx");
+        }
+        else if ((uid_txt.Text.Equals("ADMIN")) && (pwd_txt.Text.Equals("ADMIN")))
+        {
+            Response.Redirect("~/Admin/Home.aspx");
+        }
+        else
+        {
+            msg_lbl.Text = "You've Entered Invalid Login Details. Try Again";
+        }
+    }
+}
---
+++ ContactUS.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMasterPage.master" AutoEventWireup="true" CodeFile="ContactUS.aspx.cs" Inherits="ContactUS" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width:100%;">
+        <tr>
+            <td colspan="2">
+                <h2 class="title">
+                    contact us</h2>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+              
+          <p>Feel free to contact us. Your feedback or suggestion is precious to us. 
+              </p>
+    <p>You can 
+              reach us <br /><br /><b>By Mail</b><br /> Our email id : support@emandi.com / feedback@emandi.com<br /><br /><br /><br />
+              </p></td>
+        </tr>
+    </table>
+    
+</asp:Content>
+
